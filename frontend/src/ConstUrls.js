b6b98d20aeeab8e88e1d0103f2b469de2c608903
adamek195 <248955@student.pwr.edu.pl>
2021-10-02 12:26:26 +0200

    add constUrls file

---
+++ frontend/src/ConstUrls.js
@@ -0,0 +1,6 @@
+// Base
+export const BASE_URL = "https://localhost:44313/api/";
+
+// Authentication
+export const registerUrl = BASE_URL + "Account/register";
+export const authenticateUrl = BASE_URL + "Account/authenticate";

18ecf7d8cdc4e98270d5be9af051e33bdbc5559d
adamek195 <248955@student.pwr.edu.pl>
2021-10-06 15:50:13 +0200

    add get method in home component

--- frontend/src/Components/MenuComponents/Home.js
+++ frontend/src/Components/MenuComponents/Home.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 import UserNavBar from './UserNavBar';
 import HomeSideBar from './HomeSideBar';
+import axios from 'axios';
+import { userUrl } from "../../ConstUrls"
+import history from '../../History';
 
 class Home extends Component {
+
+    state = {
+        firstName: "",
+        lastName: "",
+    }
+
+    componentDidMount() {
+        this.getUser()
+    }
+
+    getUser() {
+        axios.get(userUrl)
+        .then((response) => {
+            this.setState({
+                firstName: response.data.firstName,
+                lastName: response.data.lastName,
+            })
+        })
+        .catch(() => {
+            history.push("/unauthorized");
+        })
+    }
+
     render() {
         return(
             <div>
                 <UserNavBar />
                 <HomeSideBar />
-                <h1>MENU</h1>
+                <h1>Witaj {this.state.firstName} {this.state.lastName}</h1>
             </div>
         );
     }

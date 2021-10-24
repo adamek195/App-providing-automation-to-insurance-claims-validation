ae5bb14551625dd926f4fb57fe4980738e06e7be
adamek195 <248955@student.pwr.edu.pl>
2021-10-24 13:01:54 +0200

    remove appsettings json

---
+++ backend/InsuranceApp/src/InsuranceApp.Application/Services/CarDamageDetectionService.cs
@@ -0,0 +1,13 @@
+﻿using InsuranceApp.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceApp.Application.Services
+{
+    public class CarDamageDetectionService: ICarDamageDetectionService
+    {
+    }
+}

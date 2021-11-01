10c3948755e413bca1580eaa8fff7f92f1d2bf0f
adamek195 <248955@student.pwr.edu.pl>
2021-11-01 14:42:26 +0100

    add method in gui for generating user accident documents

--- frontend/src/Components/AccidentComponents/DocumentGenerator.js
+++ frontend/src/Components/AccidentComponents/DocumentGenerator.js
@@ -13,17 +13,38 @@ class DocumentGenerator extends Component {
     state = {
         selectedPolicy: "",
         guiltyPartyAccidentId: "",
+        policies: [],
+        policyNumber: "",
+        userAccidentId: "",
         guiltyPartyAccidentDocumentError: false,
+        policyNumberError: false,
+        userAccidentDocumentError: false,
     }
 
     messages = {
-        server_error: 'Nie można wygenerować dokumentu o takim numerze zdarzenia.',
+        guiltyPartyAccident_error: 'Nie można wygenerować dokumentu o takim numerze zdarzenia.',
+        policyNumberError_error: 'Nie posiadasz polisy o takim numerze',
+        userAccident_error: 'Nie można wygenerować dokumentu o takim numerze polisy i zdarzenia.',
+    }
+
+    componentDidMount() {
+        this.getPolicies()
     }
 
     handleChangeRadioButton = (event) => {
         const value = event.target.value;
         this.setState({
             selectedPolicy: value
+        });
+    }
+
+    handleChangeUserAccidents = (event) => {
+        const value = event.target.value;
+        const name = event.target.name;
+        this.setState({
+            [name]: value,
+            policyNumberError: false,
+            userAccidentDocumentError: false,
         });
     }
 
@@ -34,6 +55,77 @@ class DocumentGenerator extends Component {
             [name]: value,
             guiltyPartyAccidentDocumentError: false,
         });
+    }
+
+    getPolicies() {
+        axios.get(policiesUrl)
+        .then((response) => {
+            this.setState({
+                policies: response.data
+            })
+        })
+        .catch(() => {
+            history.push("/unauthorized");
+        })
+    }
+
+    generateUserAccidentDocument = (e) => {
+        e.preventDefault()
+        let policy = this.state.policies.find(policy => policy.policyNumber === this.state.policyNumber)
+        if((policy === undefined) ||(policy === null)){
+            this.setState({
+                policyNumberError: true,
+            })
+        }else{
+            if(policy.typeOfInsurance === 'AC'){
+                let generateDocumentRequest = `${userAccidentsUrl}/${policy.id}/${this.state.userAccidentId}/Documents/AC`
+                axios({
+                    url: generateDocumentRequest,
+                    method: 'POST',
+                    responseType: 'blob',
+                }).then((response) => {
+                    if(response.status === 500)
+                            history.push("/internal-server-error");
+                    if(response.status === 401)
+                        history.push("/unauthorized");
+                    const url = window.URL.createObjectURL(new Blob([response.data]));
+                    const link = document.createElement('a');
+                    link.href = url;
+                    link.setAttribute('download', 'szkoda.pdf');
+                    document.body.appendChild(link);
+                    link.click();
+                })
+                .catch(() => {
+                    this.setState({
+                        userAccidentDocumentError: true
+                    })
+                });
+            }
+            else if(policy.typeOfInsurance === 'OC'){
+                let generateDocumentRequest = `${userAccidentsUrl}/${policy.id}/${this.state.userAccidentId}/Documents/OC`
+                axios({
+                    url: generateDocumentRequest,
+                    method: 'POST',
+                    responseType: 'blob',
+                }).then((response) => {
+                    if(response.status === 500)
+                            history.push("/internal-server-error");
+                    if(response.status === 401)
+                        history.push("/unauthorized");
+                    const url = window.URL.createObjectURL(new Blob([response.data]));
+                    const link = document.createElement('a');
+                    link.href = url;
+                    link.setAttribute('download', 'szkoda.pdf');
+                    document.body.appendChild(link);
+                    link.click();
+                })
+                .catch(() => {
+                    this.setState({
+                        userAccidentDocumentError: true
+                    })
+                });
+            }
+        }
     }
 
     generateGuiltyPartyAccidentDocument = () => {
@@ -96,6 +188,42 @@ class DocumentGenerator extends Component {
                         </form>
                     </div>
                 </div>
+                {this.state.selectedPolicy === "UserPolicy" &&
+                <div>
+                    <div className="accidents-document-manager-wrapper">
+                        <div className="accidents-document-manager-inner">
+                            <h4 style={{textAlign: 'center'}}>Wygeneruj dokument ze zgłoszoną szkodą z własnej polisy</h4>
+                            <div className="d-flex justify-content-center">
+                                <input name="policyNumber" style={{margin: '10px', width:'250px'}}
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Numer polisy ubezpieczeniowej"
+                                    value={this.state.policyNumber}
+                                    onChange={this.handleChangeUserAccidents}/>
+                                <p className="p-2">Nie pamietasz numer swojej polisy?</p>
+                                <Link className="nav-link p-2" to={"/policies"}>Zobacz polisy</Link>
+                            </div>
+                            {this.state.policyNumberError && <span style={{ fontSize: '15px', color: 'red' }}>{this.messages.policyNumberError_error}</span>}
+                            <div className="d-flex justify-content-center">
+                                <input name="userAccidentId" style={{margin: '10px', width:'250px'}}
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Numer szkody"
+                                    value={this.state.userAccidentId}
+                                    onChange={this.handleChangeUserAccidents}/>
+                                <p className="p-2">Nie pamietasz numeru szkody?</p>
+                                <Link className="nav-link p-2" to={"/accidents"}>Zobacz szkody</Link>
+                            </div>
+                            <div>
+                                <button type="submit" style={{margin: '10px'}}
+                                        className="btn btn-primary p-2"
+                                        onClick={this.generateUserAccidentDocument}>Wygeneruj dokument
+                                </button>
+                            </div>
+                            {this.state.userAccidentDocumentError && <span style={{ fontSize: '15px', color: 'red' }}>{this.messages.userAccident_error}</span>}
+                        </div>
+                    </div>
+                </div>}
                 {this.state.selectedPolicy === "GuiltyPartyPolicy" &&
                 <div>
                     <div className="accidents-document-manager-wrapper">
@@ -108,14 +236,16 @@ class DocumentGenerator extends Component {
                                     placeholder="Numer szkody"
                                     value={this.state.guiltyPartyAccidentId}
                                     onChange={this.handleChangeGuiltyPartyAccidents}/>
-                                <button type="submit" style={{margin: '10px'}}
-                                    className="btn btn-primary p-2"
-                                    onClick={this.generateGuiltyPartyAccidentDocument}>Wygeneruj dokument
-                                </button>
                                 <p className="p-2">Nie pamietasz numeru szkody?</p>
                                 <Link className="nav-link p-2" to={"/accidents"}>Zobacz szkody</Link>
                             </div>
-                            {this.state.guiltyPartyAccidentDocumentError && <span style={{ fontSize: '15px', color: 'red' }}>{this.messages.server_error}</span>}
+                            <div>
+                                <button type="submit" style={{margin: '10px'}}
+                                        className="btn btn-primary p-2"
+                                        onClick={this.generateGuiltyPartyAccidentDocument}>Wygeneruj dokument
+                                </button>
+                            </div>
+                            {this.state.guiltyPartyAccidentDocumentError && <span style={{ fontSize: '15px', color: 'red' }}>{this.messages.guiltyPartyAccident_error}</span>}
                         </div>
                     </div>
                 </div>}

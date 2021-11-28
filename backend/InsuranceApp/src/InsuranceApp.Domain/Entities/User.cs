283ecb15906a629f631d21f68c2e3ca04d0122a9
adamek195 <248955@student.pwr.edu.pl>
2021-11-28 17:10:38 +0100

    remove unnecessary namespaces

--- backend/InsuranceApp/src/InsuranceApp.Domain/Entities/User.cs
+++ backend/InsuranceApp/src/InsuranceApp.Domain/Entities/User.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace InsuranceApp.Domain.Entities
 {
